4b53c9c9014c9d3c54e4c7ce36f47e1d8da6e06d
kush2902 <kushagra.singh2909@gmail.com>
2026-04-19 11:15:27 +0530

    Update SideNav.jsx

--- components/SideNav.jsx
+++ components/SideNav.jsx
@@ -1,13 +1,20 @@
 "use client";
 
 export default function SideNav() {
+  const sections = ["hero", "why", "retail", "luxury", "dining", "entertainment", "events"];
+
+  const scrollTo = (id) => {
+    document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="fixed right-6 top-1/2 -translate-y-1/2 z-50">
       <div className="flex flex-col gap-4">
-        {[...Array(7)].map((_, i) => (
+        {sections.map((sec) => (
           <div
-            key={i}
-            className="w-3 h-3 bg-white/70 hover:bg-white rounded-full cursor-pointer"
+            key={sec}
+            onClick={() => scrollTo(sec)}
+            className="w-3 h-3 bg-white/60 hover:bg-white rounded-full cursor-pointer"
           />
         ))}
       </div>
